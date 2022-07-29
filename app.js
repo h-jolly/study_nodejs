32cb561dcfe8024118a11d88fdbaa32a9328b484
unknown <jprrooe@gmail.com>
2022-07-29 13:37:41 +0900

    내용 변경

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello express');
-});
-
-app.get('/jolly', (req, res) => {
-  res.send('jolly');
+  res.send('express start');
 });
 
 app.listen(port, () => {
